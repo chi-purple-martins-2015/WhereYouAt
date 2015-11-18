cb25bcde9d68f7591769c4495cd6e24852133ab9
McEileen <email": null>
2015-11-18 13:51:39 -0600

    appear to be able to access users of a group

--- WhereYouAt/app/models/grouping.rb
+++ WhereYouAt/app/models/grouping.rb
@@ -1,5 +1,5 @@
 class Grouping < ActiveRecord::Base
   belongs_to :user
-  belongs_to :user, foreign_key: :admin_id
+  belongs_to :admin, class_name: "User", foreign_key: :admin_id
   belongs_to :group
 end
--- WhereYouAt/app/models/user.rb
+++ WhereYouAt/app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
-  has_many :created_groups, class_name: "Group", foreign_key: :admin_id
+
   has_many :groupings
-  has_many :groups, through: :groupings
+  has_many :groups, through: :groupings#, foreign_key: :user_id
+  has_many :created_groups, class_name: "Group", foreign_key: :admin_id
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, format: { with: /@/, message: "Sorry, that is not a valid email address"}

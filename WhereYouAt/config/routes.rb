41e6d3d8121d61815f2fea5bef7fc1a26ce2edcd
McEileen <email": null>
2015-11-18 16:00:53 -0600

    add sessions methods

--- WhereYouAt/config/routes.rb
+++ WhereYouAt/config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
   resources :users
   resources :groups
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   post '/groupings' => 'groupings#create'
-  get '/groups/:id/map' => 'groups#show_map', as: "map"
+  get '/groups/:id/map' => 'groups#show_map'#, as: "map"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

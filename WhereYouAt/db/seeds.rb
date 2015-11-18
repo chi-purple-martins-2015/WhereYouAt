48d778d57290cc6a13e96ab36999c54e2e957e94
McEileen <email": null>
2015-11-18 10:43:36 -0600

    start seed

--- WhereYouAt/db/seeds.rb
+++ WhereYouAt/db/seeds.rb
@@ -1,3 +1,8 @@
+10.times do
+  User.create(email: Faker::Internet)
+end
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #

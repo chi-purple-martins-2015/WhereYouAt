23da4b3ca52aded513119df90c01b50bab430228
McEileen <email": null>
2015-11-18 11:35:41 -0600

    add created_groups association to user class

--- WhereYouAt/app/models/group.rb
+++ WhereYouAt/app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ActiveRecord::Base
+  has_many :groupings
   has_many :users, through: :groupings
 
 end
--- WhereYouAt/app/models/user.rb
+++ WhereYouAt/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :created_groups, class_name: "Group", foreign_key: :admin_id
+  has_many :groupings
   has_many :groups, through: :groupings
-  has_many :groups, foreign_key: :admin_id
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, format: { with: /@/, message: "Sorry, that is not a valid email address"}

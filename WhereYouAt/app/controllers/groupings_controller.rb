68d469feb5116c48e78e14f8bf36474fa3f1c0c3
McEileen <email": null>
2015-11-18 16:55:49 -0600

    readjust groupings controller when we begin working on invites

--- WhereYouAt/app/controllers/groupings_controller.rb
+++ WhereYouAt/app/controllers/groupings_controller.rb
@@ -4,8 +4,13 @@ class GroupingsController < ApplicationController
     @user = User.find_by(id: current_user)
     @group = Group.find_by(id: params[:id])
     #see if current_user is made
-    @grouping = @user.groupings.create(group_id: @group.id, user_id: @user.id, joined?: true)
+    # @grouping = @user.groupings.create(group_id: @group.id, user_id: @user.id, joined?: true)
     #redirect to group show page
+    @grouping = Grouping.where(group_id: @group.id, user_id: @user_id)
+
+    if @grouping
+      @grouping.update_attributes(joined?: true)
+    end
   end
 
   def destroy

0d17993e5160387a87435d8dac6a5155066b6145
McEileen <email": null>
2015-11-18 14:36:42 -0600

    code/psuedocode rough create method

---
+++ WhereYouAt/app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+    if @user && @user.save
+      # not sure what to put here yet because we don't have views yet
+      #create session?
+      #redirect to user's groups
+    else
+      @errors = @user.errors.full_messages
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name, :password, :phone)
+  end
+
+end

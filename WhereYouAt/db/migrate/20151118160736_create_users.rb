1bdc39a20a999fb0b3c96531481123f6ba767f7a
McEileen <email": null>
2015-11-18 10:10:43 -0600

    refine lat long fields in user migration

--- WhereYouAt/db/migrate/20151118160736_create_users.rb
+++ WhereYouAt/db/migrate/20151118160736_create_users.rb
@@ -6,8 +6,8 @@ class CreateUsers < ActiveRecord::Migration
       t.string :last_name
       t.string :password_digest
       t.string :phone
-      t.decimal :lat
-      t.decimal :lng
+      t.decimal :lat, :precision => 10, :scale => 6, :default => 0.0
+      t.decimal :lng, :precision => 10, :scale => 6, :default => 0.0
 
       t.timestamps null: false
     end

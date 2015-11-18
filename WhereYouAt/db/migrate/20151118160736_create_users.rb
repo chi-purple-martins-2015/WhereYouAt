41ee84321cf5f89cffbe1b3756a43147d404fb0c
McEileen <email": null>
2015-11-18 10:08:20 -0600

    create rails framework and initial user model

---
+++ WhereYouAt/db/migrate/20151118160736_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :first_name
+      t.string :last_name
+      t.string :password_digest
+      t.string :phone
+      t.decimal :lat
+      t.decimal :lng
+
+      t.timestamps null: false
+    end
+  end
+end
